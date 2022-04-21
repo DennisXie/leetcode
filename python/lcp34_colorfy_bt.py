5dacea2221715e842af67073102d94894099fea9
Dennis Xie <dennisxie1992@outlook.com>
2022-04-21 23:07:59 +0800

    add lcp34

---
+++ python/lcp34_colorfy_bt.py
@@ -0,0 +1,44 @@
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+
+    def maxValue(self, root: TreeNode, k: int) -> int:
+        self.k = k
+        self._cache = {
+            root: [0] * (k+1)
+        }
+        self.findMax(root, k)
+        return self._cache[root][k]
+
+    def findMax(self, node: TreeNode, leftNum: int) -> int:
+        """
+        :param node: the current traversed node
+        :param leftNum: the leftNum of nodes can be colored
+        :return:
+        """
+        if node is None:
+            return 0
+
+        if self._cache.get(node) is not None and self._cache[node][leftNum] > 0:
+            return self._cache[node][leftNum]
+
+        if self._cache.get(node) is None:
+            self._cache[node] = [0] * (self.k+1)
+
+        # current node do not colored
+        for i in range(self.k+1):
+            left = self.findMax(node.left, self.k)
+            right = self.findMax(node.right, self.k)
+            self._cache[node][leftNum] = max(self._cache[node][leftNum], left+right)
+
+        # current node colored
+        for i in range(leftNum):
+            left = self.findMax(node.left, i)
+            right = self.findMax(node.right, leftNum-i-1)
+            self._cache[node][leftNum] = max(self._cache[node][leftNum], left+right+node.val)
+        return self._cache[node][leftNum]
