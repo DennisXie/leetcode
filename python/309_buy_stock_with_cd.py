e76044c66e82b06e70500aa70579701400414a01
Dennis Xie <dennisxie1992@outlook.com>
2022-04-26 22:02:53 +0800

    add explanation of 309 solution2

--- python/309_buy_stock_with_cd.py
+++ python/309_buy_stock_with_cd.py
@@ -5,10 +5,6 @@ class Solution:
 
     def maxProfit(self, prices: List[int]) -> int:
 
-        # f[i][j][0] = max(f[i-1][j][0], f[i-1][j][2])    # hold without stock
-        # f[i][j][1] = max(f[i-1][j][1], f[i-2][j-1][0]-prices[i])    # buy or hold stock
-        # f[i][j][2] = max(f[i-1][j][1]+prices[i])                    # sell
-
         f = [[0, 0, 0] for _ in range(len(prices))]
         for i in range(len(prices)):
             if i - 1 < 0:
@@ -26,7 +22,37 @@ class Solution:
             print(i)
         return max(f[len(prices)-1][0], f[len(prices)-1][2])
 
+    def maxProfit2(self, prices: List[int]) -> int:
+
+        f = [[0, 0] for _ in range(len(prices))]
+        for i in range(len(prices)):
+            if i - 1 < 0:
+                f[i][0] = 0
+                f[i][1] = -prices[i]
+            else:
+                # Note: this is the mathematical explanation why we can use only 0 1 rather than 0, 1, 2
+                # The state transition formula is written in the maxProfit.
+                # f[i][0] = max(f[i-1][0], f[i-1][2])
+                # f[i][1] = max(f[i-1][1], f[i-1][0]-prices[i], f[i-2][2]-prices[i])
+                # f[i][2] = f[i-1][1]+prices[i]
+                # So f[i-1][2] = f[i-2][1]+prices[i-1]
+                #    f[i-2][2] = f[i-3][1]+prices[i-2]
+                #    f[i-1][0] = max(f[i-2][0], f[i-2][2]) = max(f[i-2][0], f[i-3][1]+prices[i-2])
+                #    f[i-2][0] = max(f[i-3][0], f[i-3][2]) = max(f[i-3][0], f[i-4][1]+prices[i-3])
+                # Then we can have the following:
+                # f[i][0] = max(f[i-1][0], f[i-1][2]) = max(f[i-1][0], f[i-2][1]+prices[i-1])
+                # f[i][1] = max(f[i-1][1], f[i-1][0]-prices[i], f[i-3][1]+prices[i-2]-prices[i])
+                # so we can avoid sell status. we can have new formula
+                # f[i][0/2] = max(f[i-1][0], f[i-2][1]+prices[i-1], f[i-1][1]+prices[i])
+                #           = max(f[i-1][0], f[i-1][1]+prices[i]) because f[i-1][0] = max(f[i-2][1]+prices[i-1], ...)
+                # f[i][1] = max(f[i-1][1], f[i-2][0/2]-prices[i]) because f[i-2][0/2] = max(f[i-3][1]+prices[i-2], ...)
+
+                f[i][0] = max(f[i-1][0], f[i-1][1]+prices[i])
+                f[i][1] = max(f[i-1][1], f[i-2][0]-prices[i])
+
+        return f[len(prices)-1][0]
+
 
 if __name__ == "__main__":
-    print(Solution().maxProfit([1]))
-    print(Solution().maxProfit([1, 2, 3, 0, 2]))
+    print(Solution().maxProfit2([1]))
+    print(Solution().maxProfit2([1, 2, 3, 0, 2]))
